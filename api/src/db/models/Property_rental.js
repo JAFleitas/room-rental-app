1e41d115c9376c7052772271b46b99badc5e9e3c
blaksleyn <blaksleyn@gmail.com>
2022-03-15 19:28:09 -0300

    Creacion de modelo Alquiler_propiedad

--- api/src/db/models/Property_rental.js
+++ api/src/db/models/Property_rental.js
@@ -1,40 +1,44 @@
 const { DataTypes } = require("sequelize")
 
 module.exports = sequelize => {
-  sequelize.define("property_rental", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
+  sequelize.define(
+    "property_rental",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      userID: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      porpertyID: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      final_price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      statusPropertyId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      rental_dates: {
+        type: DataTypes.DATE,
+        allownull: false,
+      },
+      start_date: {
+        type: DataTypes.DATE,
+        allownull: false,
+      },
+      final_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
     },
-    userID: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    porpertyID: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    final_price: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    statusPropertyId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    rental_dates: {
-      type: DataTypes.DATE,
-      allownull: false,
-    },
-    start_date: {
-      type: DataTypes.DATE,
-      allownull: false,
-    },
-    final_date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-  })
+    { timestamps: false },
+  )
 }
