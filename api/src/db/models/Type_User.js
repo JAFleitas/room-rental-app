6d74fd8fc7ae65327c985ced8699c2037ab59820
Luis <blackkndd@gmail.com>
2022-03-15 17:06:25 -0500

    creando modelo type_user y modificando pk y fk a UUID

---
+++ api/src/db/models/Type_User.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize")
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = sequelize => {
+  // defino el modelo
+  sequelize.define(
+    "type_user",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      Nombre: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+        unique: true,
+      },
+    },
+    { timestamps: false },
+  )
+}
