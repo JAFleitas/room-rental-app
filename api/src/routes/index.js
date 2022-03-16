9f37f2a06cbe1787e778fcb24a2bb50d33cb67f6
blaksleyn <blaksleyn@gmail.com>
2022-03-16 16:10:05 -0300

    Creacion de ruta crear alquiler

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,15 @@
-const { Router } = require("express");
+const { Router } = require("express")
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const PropertyRoutes = require("./PropertyRoutes"); 
+const PropertyRoutes = require("./PropertyRoutes")
+const rentalRoutes = require("./rentalRoutes")
 
-const router = Router();
+const router = Router()
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.use("/app", PropertyRoutes);
+router.use("/app", PropertyRoutes)
+router.use("/app", rentalRoutes)
 
-module.exports = router;
+module.exports = router
