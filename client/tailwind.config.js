e7f6835724cfd7614a46c21cc62cfd9d25c4643b
Thdalessa <89609966+Thdalessa@users.noreply.github.com>
2022-03-16 14:57:37 -0300

    Se borraron partes inutiles comentadas

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,3 @@
-// module.exports = {
-//   content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
