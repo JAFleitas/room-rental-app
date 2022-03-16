f4aa9bcc4448e8a2ed01e619a4e5437617f35994
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-03-16 18:18:40 -0300

    haciendo cambios en routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,13 @@
-import React from "react"
+import React, { StrictMode } from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./index.css"
 import App from "./App"
-import Details from "./pages/details-property/detailsProperty"
+import RouterApp from "./routes/RouterApp"
 
 ReactDOM.render(
-
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}></Route>
-    </Routes>
-  </BrowserRouter>,
-
- 
-  
-
+  <StrictMode>
+    <App />
+    <Rogit uterApp />
+  </StrictMode>,
   document.getElementById("root"),
 )
--- client/src/routes/RouterApp.jsx
+++ client/src/routes/RouterApp.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React from "react"
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Details from "../pages/details-property/detailsProperty.jsx"
 import PropertyCard from "../components/PropertyCard/PropertyCard.jsx"
 
 const RouterApp = () => {
-    
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PropertyCard />} />
+        <Route path="/property-info" element={<Details />} />
       </Routes>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default RouterApp;
+export default RouterApp
