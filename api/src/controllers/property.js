20a8052131eed88910b84e2c04ed9c485b58892c
Viviana <vivicalvat007@gmail.com>
2022-03-16 12:19:42 -0500

    Añadiendo busquedas, ordenamientos y configuracion de atributos en ruta GET /properties

--- api/src/controllers/property.js
+++ api/src/controllers/property.js
@@ -1,17 +1,61 @@
 const { Property } = require("../db")
+const { Op } = require("sequelize")
 
 const getAll = async (req, res, next) => {
   try {
-    const { attributes, sort, filters } = req.query
+    let {
+      attributes,
+      order = "ASC",
+      orderBy = "name",
+      search,
+      exclude,
+    } = req.query
 
-    let propiedades = await Property.findAll({
-      attributes: { exclude: ["description", "numberOfReviews", "userID"] },
-    })
-    if (propiedades.length === 0) {
+    // Definimos que atributos quiere traer o excluir
+    if (attributes || exclude) {
+      attributes
+        ? (attributes = attributes.split(" "))
+        : (attributes = { exclude: exclude.split(" ") })
+    }
+
+    // Definiendo el ordenamiendo: default por nombre ascendentemente
+    orderBy = orderBy.split(" ")
+    orderBy.push(order)
+    let ordenamiento = [orderBy]
+
+    // Definimos si hay un termino de búsqueda
+    if (search) {
+      search = {
+        name: {
+          [Op.iLike]: `%${search}%`,
+        },
+      }
+    }
+
+    let properties
+
+    if (search) {
+      properties = await Property.findAll({
+        attributes: attributes || {
+          exclude: ["description", "userID"],
+        },
+        order: ordenamiento,
+        where: search,
+      })
+    } else {
+      properties = await Property.findAll({
+        attributes: attributes || {
+          exclude: ["description", "userID"],
+        },
+        order: ordenamiento,
+      })
+    }
+
+    if (properties.length === 0) {
       return next({ message: "Properties not founded", status: 404 })
     }
 
-    res.json(propiedades)
+    res.json(properties)
   } catch (error) {
     next(error)
   }
