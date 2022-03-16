4cf9ac0df3c84917f441c7494bfb7adf07be5837
Thdalessa <thdalessa186@gmail.com>
2022-03-16 17:39:40 -0300

    Agrega un Footer, Colores al Nav y se corrije la altura del contenedor App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,13 @@ import logo from "./logo.svg"
 import "./App.css"
 import RouterApp from "./routes/RouterApp"
 import Nav from "./components/Nav/Nav"
-
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Footer />
     </div>
   )
 }
