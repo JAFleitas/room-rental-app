cdf457c17f2c3735d72ae9ee2464fc8118bc62ff
blaksleyn <blaksleyn@gmail.com>
2022-03-17 13:45:39 -0300

    Cambios en la base de datos y agreggue modelo de servicios

--- api/src/db/models/property.js
+++ api/src/db/models/property.js
@@ -1,61 +1,81 @@
 const { DataTypes } = require("sequelize")
 
 module.exports = sequelize => {
-  sequelize.define("property", 
-  {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
+  sequelize.define(
+    "property",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      location: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+      numberOfRooms: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      maxNumberOfPeople: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      rating: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.ARRAY(DataTypes.JSON),
+        allowNull: false,
+      },
+      userID: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      coordinates: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      flat: {
+        type: DataTypes.INTEGER,
+      },
+      discount: {
+        type: DataTypes.INTEGER,
+      },
+      wifi: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      air_conditioning: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      id_service: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      id_type_property: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
     },
-    name: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    location: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.DOUBLE,
-      allowNull: false,
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    numberOfRooms: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    maxNumberOfPeople: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    rating: {
-      type: DataTypes.DOUBLE,
-      allowNull: false,
-    },
-    numberOfReviews: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-    },
-    image: {
-      type: DataTypes.ARRAY(DataTypes.JSON),
-      allowNull: false,
-    },
-    userID: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-    },
-  },                  
     { timestamps: false },
- )
+  )
 }
