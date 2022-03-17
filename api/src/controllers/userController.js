b9d448a2c0d031aa3da13ca8f04227774e2ac021
Viviana <vivicalvat007@gmail.com>
2022-03-17 14:55:41 -0500

    Completando ruta login y  añadiendo validaciones por campos faltantes

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -4,15 +4,18 @@ const { JWT_SECRET } = process.env
 
 const createUser = async (req, res, next) => {
   const {
-  name,
-  lastname,
-  country,
-  email,
-  city,
-  password,
-  credit_card_number,
-} = req.body;
+    name,
+    lastname,
+    country,
+    email,
+    city,
+    password,
+    credit_card_number,
+  } = req.body;
 
+  if (!name || !lastname || !country || !email || !city || !password){
+    return next({status: 400, message: "All fields are required"});
+  }
   try {
     let user = await User.findOne({ where: { email } })
 
@@ -22,7 +25,16 @@ const createUser = async (req, res, next) => {
     }
 
     // Creamos el nuevo usuario y lo guardamos en la DB
-    user = await User.create(name, lastname, country, email, city, password, credit_card_number);
+    user = await User.create({
+      name,
+      lastname,
+      country,
+      email,
+      city,
+      password,
+      credit_card_number,
+      status: "enabled",
+    })
 
     // generamos el payload/body para generar el token
     const payload = {
