36fd09cc1f7df8c1f27fc0a61f36c5b2cfd18006
Facundo Sanchez <78142040+facundo9963@users.noreply.github.com>
2022-03-17 17:59:43 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css"
 import Nav from "./components/Nav/Nav"
 import Footer from "./components/Footer/Footer"
-
+import RouterApp from "./routes/RouterApp"
 
 function App() {
 
@@ -21,7 +21,7 @@ function App() {
   return (
     <div className="App">
       <Nav />
-
+         <RouterApp/>
       <Footer />
     </div>
   )
