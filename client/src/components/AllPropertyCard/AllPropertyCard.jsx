e6f72c2204a4d2e318d1d58cdc4946f4936b8ad8
Facundo Sanchez <78142040+facundo9963@users.noreply.github.com>
2022-03-17 19:55:49 -0300

    Update AllPropertyCard.jsx

--- client/src/components/AllPropertyCard/AllPropertyCard.jsx
+++ client/src/components/AllPropertyCard/AllPropertyCard.jsx
@@ -1,1 +1,32 @@
 import React from "react"
+import React from "react"
+import PropertyCard from "../PropertyCard/PropertyCard"
+
+// aca hay que agregar el useEfect y además traernos las propiedades del estado
+//global a una variable (allPropierties) y mapearla para renderizar cada propiedad
+
+function AllPropertyCard() {
+    return (
+      <Cards>
+        {
+            allPropierties && allPropierties.map((property)=>{
+                return(
+                    <PropertyCard
+                    key= {property.id}
+                    name={property.name}
+                    location={property.location}
+                    price={property.price}
+                    numberOfRooms={property.numberOfRooms}
+                    maxNumberOfPeople={property.maxNumberOfPeople}
+                    description={property.description}
+                    rating={property.rating}
+                    image={property.image}
+                    />
+                )
+            })
+        }
+      </Cards>
+    )
+  }
+  
+  export default AllPropertyCard
