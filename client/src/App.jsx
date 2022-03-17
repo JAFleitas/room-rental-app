a04e7228345479949b4736e7da30a4a186b15442
Facundo Sánchez <facundosanchez9963@gmail.com>
2022-03-17 04:02:04 -0300

    Agregue carrousel en la propertyCard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,32 @@ import "./App.css"
 import { Routes, Route } from "react-router-dom"
 import Nav from "./components/Nav/Nav"
 import Footer from "./components/Footer/Footer"
+import PropertyCard from "./components/PropertyCard/PropertyCard"
 import Details from "./pages/details-property/detailsProperty"
 import AboutUs from "./pages/about-us/AboutUs"
 
 function App() {
+
+  // const house={
+  //   price:15,
+  //   title:"casa frente al mar",
+  //   info:"3baños,2habitaciones,comedor, cocina, desayuno",
+  //   rating:4.5,
+  //   images:["https://decoraideas.com/wp-content/uploads/2019/08/04-4-768x536.jpg",
+  //   "https://decoraideas.com/wp-content/uploads/2019/08/04-4-768x536.jpg",
+  //   "https://decoraideas.com/wp-content/uploads/2019/08/04-4-768x536.jpg",
+    
+
+  // ]
+
+  // }
   return (
     <div className="App">
       <Nav />
       <Routes>
         <Route exact path="/" element={<div>Home</div>}></Route>
         <Route exact path="/about-us" element={<AboutUs />}></Route>
+        <Route exact path="/properties" element={<PropertyCard/>}></Route>
       </Routes>
       <Footer />
     </div>
--- client/src/components/PropertyCard/PropertyCard.jsx
+++ client/src/components/PropertyCard/PropertyCard.jsx
@@ -1,18 +1,51 @@
 import React from "react"
-import {AiFillStar } from 'react-icons/ai';
+import { AiFillStar } from "react-icons/ai"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import {
+  Container,
+  Image,
+  Title,
+  Price,
+  ImageContainer,
+  Info,
+  Rating,
+  DivPyR,
+} from "./styled"
 
-import { Container, Image, Title, Price, ImageContainer, Info, Rating } from "./styled"
-
-function PropertyCard({property}) {
+import Slider from "react-slick"
+function PropertyCard({ property }) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
   return (
     <Container>
-      <Title>{property.title}</Title>
       <ImageContainer>
-        <Image src={property.image}></Image>
+        <Slider {...settings}>
+          {property.images &&
+            property.images.map((image, i) => {
+              return (
+                <div key={i}>
+                  <Image src={image}></Image>
+                </div>
+              )
+            })}
+        </Slider>
       </ImageContainer>
+            <Title>{property.title}</Title>
       <Info>{property.info}</Info>
+      <DivPyR>
+
       <Price> Price: {property.price}/ night </Price>
-      <Rating><AiFillStar color="pink" />{property.rating}</Rating>
+      <Rating>
+        <AiFillStar color="pink" />
+        {property.rating}
+      </Rating>
+      </DivPyR>
     </Container>
   )
 }
