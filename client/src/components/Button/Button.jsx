5ed29fc39dbbafd671550dd28507064b22c99ba3
Thdalessa <thdalessa186@gmail.com>
2022-03-18 12:00:21 -0300

    Se agrega un componente DropDown

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,18 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 import { AiOutlineMenu } from "react-icons/ai"
 import { FaRegUserCircle } from "react-icons/fa"
-
 import { Container, Icon, ImageContainer } from "./styled"
+import DropDown from "../DropDown/DropDown"
 
 export default function Button() {
+  const [open, setOpen] = useState(false)
+
   return (
-    <Container>
+    <Container onClick={() => setOpen(!open)}>
       <Icon>
         <AiOutlineMenu />
       </Icon>
       <ImageContainer>
         <FaRegUserCircle />
       </ImageContainer>
+      {open && <DropDown />}
     </Container>
   )
 }
