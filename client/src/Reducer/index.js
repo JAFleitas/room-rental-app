ebc3953b48965d88bc088f99528e39b4f3cfaaf6
blaksleyn <blaksleyn@gmail.com>
2022-03-18 19:17:00 -0300

    Cree el store y el reducer

---
+++ client/src/Reducer/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+  propertys: [],
+  propertys2: [],
+  detailsOfPropertys: [],
+}
+
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default rootReducer
