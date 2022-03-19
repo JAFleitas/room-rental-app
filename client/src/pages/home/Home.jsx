f50222dbe69d12b8c82aa40c0f5c74c401f3d6b7
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-03-19 14:46:25 -0300

    saque el seachBar de la home y deje comentado el link a formulario

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,14 +1,12 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import SearchBar from "../../components/SearchBar/SearchBar"
 import AllPropertyCard from "../../components/AllPropertyCard/AllPropertyCard"
 
 export default function Home() {
   return (
     <div>
-      <SearchBar />
-      <AllPropertyCard/>
-      <Link to="/form"> <button> new rental </button> </Link>
+      <AllPropertyCard />
+      {/*  <Link to="/form"> <button> new rental </button> </Link> */}
     </div>
   )
 }
