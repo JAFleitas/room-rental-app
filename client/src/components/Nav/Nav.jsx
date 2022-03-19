8f06242cb86ec4d9e6d7a5747de971196ee55a55
emaromerot <emaromeroies17@gmail.com>
2022-03-18 21:36:42 -0300

    comenzando form

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Button from "../Button/Button"
 import { Navigator, IconContainer, Logo, Title, ToHome } from "./styled"
 import logo from "../../assets/Rental_App_Logo.png"
+import { Link } from "react-router-dom"
 
 export default function Nav() {
   function onClick(e) {
@@ -17,6 +18,7 @@ export default function Nav() {
         <Title>Rental App</Title>
       </IconContainer>
       <Button />
+      
     </Navigator>
   )
 }
--- client/src/routes/RouterApp.jsx
+++ client/src/routes/RouterApp.jsx
@@ -5,7 +5,7 @@ import Details from "../pages/details-property/detailsProperty.jsx"
 import PropertyCard from "../components/PropertyCard/PropertyCard.jsx"
 import AboutUs from "../pages/about-us/AboutUs"
 import Home from "../pages/home/Home.jsx"
-
+import Form from "../components/Form/Form.jsx"  
 const RouterApp = () => {
   return (
     <Routes>
@@ -13,6 +13,8 @@ const RouterApp = () => {
       <Route path="/property-info" element={<Details />} />
       <Route exact path="/" element={<Home />}></Route>
       <Route exact path="/about-us" element={<AboutUs />}></Route>
+      <Route exact path="/form" element={<Form/>} > </Route>
+
     </Routes>
   )
 }
