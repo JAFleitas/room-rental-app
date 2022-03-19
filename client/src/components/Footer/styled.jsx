dd12db1991b388d7213b94b3941772e2ce3e59c8
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-03-19 17:17:00 -0300

    modificando estilos en footer

--- client/src/components/Footer/styled.jsx
+++ client/src/components/Footer/styled.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import { Link } from "react-router-dom"
+/* import { Link } from "react-router-dom"
 
 export const FooterDiv = styled.footer`
   height: 25vh;
@@ -44,16 +44,7 @@ export const TeamName = styled(Link)`
   }
 `
 
-export const LinkGithubProject = styled.a`
-  text-decoration: none;
-  font-size: 35px;
-  padding: 8px;
-  border-radius: 10px;
 
-  &:hover {
-    background-color: #e0aaff;
-  }
-`
 
 export const FooterOptions = styled.a`
   text-decoration: none;
@@ -66,5 +57,47 @@ export const FooterOptions = styled.a`
 
   &:hover {
     background-color: #e0aaff;
+  }
+` */
+export const LinkGithubProject = styled.a`
+  text-decoration: none;
+`
+
+export const Container = styled.div`
+  color: #e9e0e0ee;
+  background-color: #0f1111;
+  padding-top: 15px;
+  padding-bottom: 15px;
+`
+
+export const SubWrapper = styled.div`
+  display: grid;
+  grid-template-columns: minmax(210px, 1000px);
+  justify-content: center;
+`
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(180px, auto));
+`
+
+export const Card = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-self: end;
+  margin: 20px;
+  width: 130px;
+`
+export const CardTitle = styled.h1`
+  margin-bottom: 10px;
+`
+export const CardLink = styled.li`
+  list-style-type: none;
+  cursor: pointer;
+  padding: 5px;
+  transition: all 0.5s ease;
+  :hover {
+    padding-left: 15px;
+    color: #e9e0e0ee;
   }
 `
