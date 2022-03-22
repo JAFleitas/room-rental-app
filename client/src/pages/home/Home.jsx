e00bba63920fbf6ae8d9eb6f075cccf43f1c8345
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-03-22 10:45:55 -0300

    cambiando filtros a home

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import AllPropertyCard from "../../components/AllPropertyCard/AllPropertyCard"
+import Sorters from "../../components/Filters/Sorters"
 import Paginated from "../../components/paginated/paginated"
 
 export default function Home() {
   return (
     <div>
+      <Sorters />
       <AllPropertyCard />
       <Paginated />
     </div>
