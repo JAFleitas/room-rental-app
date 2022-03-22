00af2a762dd6250fe3a58c3e175d9c717bdebb2c
Facundo Sánchez <facundosanchez9963@gmail.com>
2022-03-22 03:53:22 -0300

    Agregado el useEffect, todo funciona(por las dudas probar despues)

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,14 +1,18 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Button from "../Button/Button"
 import SearchBar from "../SearchBar/SearchBar"
 import { Navigator, IconContainer, Logo, ToHome } from "./styled"
 import logo from "../../assets/Rental_App_Logo.png"
-import { Link } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import {loadUser} from "../../redux/actions/index"
+
 
 export default function Nav() {
-  function onClick(e) {
-    e.preventDefault()
-  }
+
+  const dispatch= useDispatch();
+  useEffect(() => {
+    dispatch(loadUser())
+  }, [])
 
   return (
     <Navigator>
