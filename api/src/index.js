ec4a9e72cad1ccc4eac9211007337a0123152369
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-03-25 19:55:57 -0300

    empezando a hacer el deploy

--- api/src/index.js
+++ api/src/index.js
@@ -2,13 +2,13 @@ const initDB = require("./utilities/initDB")
 const app = require("./app")
 const { sequelize } = require("./db/index.js")
 
-const { PORT } = process.env
+const PORT = process.env.PORT
 // Syncing all the models at once.
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`) // eslint-disable-line no-console
 
     // Inicializamos la DDBB
-    await initDB();
+    await initDB()
   })
 })
