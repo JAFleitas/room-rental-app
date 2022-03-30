c0f20f0c3161f01df228cc2ab3144419b1b32a2e
Viviana <vivicalvat007@gmail.com>
2022-03-30 17:49:00 -0500

    resolve conflict

--- client/src/routes/RouterApp.jsx
+++ client/src/routes/RouterApp.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom"
 import Details from "../pages/details-property/detailsProperty.jsx"
 import AboutUs from "../pages/about-us/AboutUs"
 import Home from "../pages/home/Home.jsx"
-
 import { AddProperty } from "../components/formAddProperty/addProperty.jsx"
 import Profile from "../pages/profile/profile.jsx"
 import Error404 from "../pages/404/Error404.jsx"
@@ -15,7 +14,6 @@ import Help from "../pages/Help/Help.jsx"
 import { EditProperty } from "../components/formAddProperty/editProperty.jsx"
 import Emails from "../pages/AdminDashboard/Emails.jsx"
 import FormEmail from "../pages/AdminDashboard/FormEmail.jsx"
-
 
 const RouterApp = () => {
   return (
@@ -29,17 +27,16 @@ const RouterApp = () => {
       <Route exact path="/signUp" element={<SignUp />} />
       <Route exact path="/profile/*" element={<Profile />} />
       <Route path="/add/payment-method" element={<FormPaymentMethod />} />
-      <Route path="/edit/payment-method/:id" element={<FormPaymentMethod edit={true} />} />
-      <Route path="/forgot-password" element={<Forgotpassword/>} />
-      <Route path="/help" element={<Help/>} />
+      <Route
+        path="/edit/payment-method/:id"
+        element={<FormPaymentMethod edit={true} />}
+      />
+      <Route path="/forgot-password" element={<Forgotpassword />} />
+      <Route path="/help" element={<Help />} />
       <Route path="/dashboard">
-<<<
-        <Route path="emails" element = {<Emails />}/>
-        <Route path="emails/create" element = {<FormEmail />}/>
-        <Route path="emails/resend/:id" element = {<FormEmail resend={true} />}/>
-=======
-      <Route path="emails" element = {<Emails />}/>
-
+        <Route path="emails" element={<Emails />} />
+        <Route path="emails/create" element={<FormEmail />} />
+        <Route path="emails/resend/:id" element={<FormEmail resend={true} />} />
       </Route>
       <Route path="/*" element={<Error404 />} />
       {/* <Route exact path="favorites" element={<Favorites />}></Route> */}
