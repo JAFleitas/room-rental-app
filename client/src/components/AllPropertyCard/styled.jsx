0cc24442679736d5b4458205fa6b986370b62e4d
Viviana <vivicalvat007@gmail.com>
2022-04-02 14:36:47 -0500

    Quitando espacio sobrante en all cards

--- client/src/components/AllPropertyCard/styled.jsx
+++ client/src/components/AllPropertyCard/styled.jsx
@@ -6,7 +6,6 @@ export const Card = styled.div`
   width: 100%;
   padding: 1rem;
   margin-right: 1rem;
-  min-height: 1134px;
   align-content: flex-start;
   @media screen and (max-width: 700px) {
     flex-direction: column;
