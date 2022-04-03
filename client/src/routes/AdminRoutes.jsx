88b95ff6bc3df52ed12e5b7b4237eaa1aa374ecb
Thdalessa <thdalessa186@gmail.com>
2022-04-03 15:09:58 -0300

    Se agrega una LandingPage pero no esta enrutada...

--- client/src/routes/AdminRoutes.jsx
+++ client/src/routes/AdminRoutes.jsx
@@ -6,21 +6,26 @@ import FormEmail from "../pages/AdminDashboard/FormEmail.jsx"
 import Users from "../pages/AdminDashboard/Users.jsx"
 import LeftBar from "../pages/AdminDashboard/LeftBar.jsx"
 import FormUser from "../pages/AdminDashboard/FormUser.jsx"
+import LandingPage from "../pages/LandingPage/LandingPage"
 
 const AdminRoutes = () => {
   return (
     <>
-    <LeftBar />
-    <Routes>
-      <Route path="/dashboard">
-        <Route path="emails" element={<Emails />} />
-        <Route path="emails/create" element={<FormEmail />} />
-        <Route path="emails/resend/:id" element={<FormEmail resend={true} />} />
-        <Route path="users" element={<Users />} />
-        <Route path="users/actions/:id" element={<FormUser />} />
-      </Route>
+      <LeftBar />
+      <Routes>
+        <Route path="/dashboard">
+          <Route path="emails" element={<Emails />} />
+          <Route path="emails/create" element={<FormEmail />} />
+          <Route
+            path="emails/resend/:id"
+            element={<FormEmail resend={true} />}
+          />
+          <Route path="users" element={<Users />} />
+          <Route path="users/actions/:id" element={<FormUser />} />
+          <Route path="landing-page" element={<LandingPage />} />
+        </Route>
         <Route path="*" element={<Error404 />} />
-    </Routes>
+      </Routes>
     </>
   )
 }
