72991b2ebc68afc6f3799f0426667fdeefdd96a3
Viviana <vivicalvat007@gmail.com>
2022-04-03 18:38:58 -0500

    Fixing stars in reviews property

--- client/src/pages/details-property/components/reviewCard/reviewCard.jsx
+++ client/src/pages/details-property/components/reviewCard/reviewCard.jsx
@@ -1,23 +1,19 @@
 import {
   ContainerCard,
   ContainerComment,
-  ContainerStars,
-  FillStarIcon,
-  // Title,
 } from "./styles"
+import Rating from "@mui/material/Rating";
 
 export default function ReviewCard({ comment }) {
-  let stars = [];
 
-  for (let star = 0; star < comment.rating; star++) {
-    stars.push(<FillStarIcon key={star} />)
-  }
-  // console.log(stars);
   return (
     <ContainerCard>
-      <ContainerStars>
-        {stars}
-      </ContainerStars>
+      <Rating
+        name="read-only"
+        value={comment.rating}
+        precision={0.5}
+        readOnly
+      />
       <ContainerComment>
         <p>{comment.message}</p>
         <span>Comment on: {comment.creationDate}</span>
