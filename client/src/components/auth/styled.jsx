8227ff97d2ccc033b23ad16024df85f4edb9201b
Thdalessa <thdalessa186@gmail.com>
2022-04-04 18:46:33 -0300

    Se hace responsive el DropDown

---
+++ client/src/components/auth/styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const LogOutOption = styled.span`
+  width: 100%;
+  text-align: left;
+  @media (max-width: 550px) {
+    text-align: center;
+  }
+`
