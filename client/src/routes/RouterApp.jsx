c559534ed38c696565665ef940d1471bdd939ccd
Viviana <vivicalvat007@gmail.com>
2022-04-05 12:36:52 -0500

    Fixing redirections to subadmin dashboard

--- client/src/routes/RouterApp.jsx
+++ client/src/routes/RouterApp.jsx
@@ -3,12 +3,15 @@ import { useSelector } from "react-redux"
 import AdminRoutes from "./AdminRoutes"
 import UserRoutes from "./UserRoutes"
 
-
 const RouterApp = () => {
   const admin = useSelector(state => state.user.type)
   const auth = useSelector(state => state.auth)
 
-  return auth && admin === "ADMIN" ? <AdminRoutes /> : <UserRoutes />
+  return auth && (admin === "ADMIN" || admin === "SUBADMIN") ? (
+    <AdminRoutes />
+  ) : (
+    <UserRoutes />
+  )
 }
 
 export default RouterApp
