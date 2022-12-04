89418127cbb732f7dec8925a6d0e9fb0c7e1a62b
Gonzalo Damián Fleitas <88991463+JAFleitas@users.noreply.github.com>
2022-12-04 10:58:27 -0300

    Update index.js
    modificando puerto de postgres

--- api/src/db/index.js
+++ api/src/db/index.js
@@ -5,7 +5,7 @@ const { Sequelize, Op } = require("sequelize")
 const fs = require("fs")
 const path = require("path")
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env
 
 /* eslint-disable */
 const sequelize =
